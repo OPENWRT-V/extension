82d768522b4b78b08e3c01c246e0e05c2643161b
zhangtian <zhangtian1993@hotmail.com>
2023-02-03 19:12:14 +0800

    Dev: 优化脚本

--- preinstall-vlmcsd/preinstall.sh
+++ preinstall-vlmcsd/preinstall.sh
@@ -3,6 +3,7 @@
 log="/usr/preinstall/vlmcsd/run.log"
 ipk1="/usr/preinstall/vlmcsd/vlmcsd.ipk"
 ipk2="/usr/preinstall/vlmcsd/luci-app-vlmcsd.ipk"
+conf="/usr/preinstall/vlmcsd/vlmcsd.conf"
 
 function now() {
     echo $(date +'%Y-%m-%d %H:%M:%S')
@@ -31,6 +32,18 @@ divider
 echo "[$(now)] Install $ipk2" >> $log
 newline
 opkg install $ipk2 >> $log 2>&1
+
+divider
+
+echo "[$(now)] Configure $ipk2" >> $log
+newline
+cp -rf $conf /etc/config/vlmcsd >> $log 2>&1
+
+divider
+
+echo "[$(now)] Restart Server" >> $log
+newline
+/etc/init.d/vlmcsd restart
 
 divider
 
