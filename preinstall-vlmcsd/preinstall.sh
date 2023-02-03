eb66a3a187ea6f694f56116f4dad0778c60b1fe2
zhangtian <zhangtian1993@hotmail.com>
2023-02-03 21:56:32 +0800

    Feat: 去除预装文件

--- preinstall-vlmcsd/preinstall.sh
+++ preinstall-vlmcsd/preinstall.sh
@@ -3,7 +3,6 @@
 log="/usr/preinstall/vlmcsd/run.log"
 ipk1="/usr/preinstall/vlmcsd/vlmcsd.ipk"
 ipk2="/usr/preinstall/vlmcsd/luci-app-vlmcsd.ipk"
-conf="/usr/preinstall/vlmcsd/vlmcsd.conf"
 
 function now() {
     echo $(date +'%Y-%m-%d %H:%M:%S')
@@ -32,18 +31,6 @@ divider
 echo "[$(now)] Install $ipk2" >> $log
 newline
 opkg install $ipk2 >> $log 2>&1
-
-divider
-
-echo "[$(now)] Configure $ipk2" >> $log
-newline
-cp -rf $conf /etc/config/vlmcsd >> $log 2>&1
-
-divider
-
-echo "[$(now)] Restart Server" >> $log
-newline
-/etc/init.d/vlmcsd restart >> $log 2>&1
 
 divider
 
