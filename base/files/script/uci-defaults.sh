9c2653c6efe74aa1b61f97f6e9915a8257aaf584
zhangtian <zhangtian1993@hotmail.com>
2023-02-21 23:23:08 +0800

    Dev: debug

--- base/files/script/uci-defaults.sh
+++ base/files/script/uci-defaults.sh
@@ -1,6 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 echo "uci-debug" >> /dev/kmsg
-shopt >> /dev/kmsg
-source /usr/extension/base/library.sh >> /dev/kmsg 2>&1
-execJob "base" "setup" >> /dev/kmsg 2>&1
+bash -c "source /usr/extension/base/library.sh && execJob base setup"
